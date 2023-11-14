fd80deca9401a018b18deae3c9bfdbf5bee9a9c8
by-westik <nastya.derevyanko.2020@mail.ru>
2023-11-15 01:40:49 +0600

    Add AuthFragment simple logic

--- app/src/main/java/com/cocktail/party/AuthFragment.kt
+++ app/src/main/java/com/cocktail/party/AuthFragment.kt
@@ -1,6 +1,47 @@
 package com.cocktail.party
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.cocktail.party.databinding.FragmentAuthBinding
+import com.google.android.material.button.MaterialButton
 
 class AuthFragment : Fragment() {
+
+    private var _binding: FragmentAuthBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private lateinit var btnLogin: MaterialButton
+    private lateinit var tvSignUp: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater)
+
+        btnLogin = binding.btnLogin
+        tvSignUp = binding.tvSwitchToSignup
+
+        btnLogin.setOnClickListener {
+            val email = binding.loginText.text.toString()
+            val password = binding.passwordText.text.toString()
+            /*
+            TODO добавить логику автоизации
+             */
+        }
+
+        tvSignUp.setOnClickListener {
+            /*
+            TODO добавить навигацию на фрагмент регистрации
+             */
+        }
+
+        return binding.root
+    }
 }
