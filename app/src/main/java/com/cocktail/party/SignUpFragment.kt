185f33d7e82050bf47d18fd2504bdea41d0dd431
by-westik <nastya.derevyanko.2020@mail.ru>
2023-11-15 01:58:15 +0600

    Add simple SignUpFragment

---
+++ app/src/main/java/com/cocktail/party/SignUpFragment.kt
@@ -0,0 +1,48 @@
+package com.cocktail.party
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.cocktail.party.databinding.FragmentSignUpBinding
+import com.google.android.material.button.MaterialButton
+
+class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private lateinit var btnSignUp: MaterialButton
+    private lateinit var tvAuth: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSignUpBinding.inflate(inflater)
+
+        btnSignUp = binding.signUpBtn
+        tvAuth = binding.tvSwitchToAuth
+
+        btnSignUp.setOnClickListener {
+            val email = binding.emailText.text.toString()
+            val login = binding.loginText.text.toString()
+            val password = binding.passwordText.text.toString()
+            /*
+            TODO добавить логику регистрации
+             */
+        }
+
+        tvAuth.setOnClickListener {
+            /*
+            TODO добавить навигацию на фрагмент авторизации
+             */
+        }
+
+        return binding.root
+    }
+}
