ff846ccabc4e3d0b554243b7dcf9c0913c972323
by-westik <nastya.derevyanko.2020@mail.ru>
2023-11-15 17:42:53 +0600

    Add base classes for work with network

---
+++ app/src/main/java/com/cocktail/party/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.cocktail.party.utils
+
+object Constants {
+    /*
+    TODO необходимо добавить интерфейс чтобы можно было задавать ip адрес в самом приложении
+     */
+    const val BASE_URL = "http://192.168.0.106:8080"
+
+    const val LOGIN_END_POINT = "login"
+    const val REGISTER_END_POINT = "create_user"
+}
